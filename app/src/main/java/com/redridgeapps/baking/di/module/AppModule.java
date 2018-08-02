4051f1e058b71378ec0233da69cb3d325d04b51a
Saurabh Sandav <justmailsaurabh@gmail.com>
2018-08-02 14:25:15 +0530

    AppModule: Provide Network Dependencies
    Signed-off-by: Saurabh Sandav <justmailsaurabh@gmail.com>

--- app/src/main/java/com/redridgeapps/baking/di/module/AppModule.java
+++ app/src/main/java/com/redridgeapps/baking/di/module/AppModule.java
@@ -1,7 +1,35 @@
 package com.redridgeapps.baking.di.module;
 
+import com.redridgeapps.baking.api.RecipeService;
+
+import javax.inject.Singleton;
+
 import dagger.Module;
+import dagger.Provides;
+import retrofit2.Retrofit;
+import retrofit2.converter.moshi.MoshiConverterFactory;
 
 @Module
 public abstract class AppModule {
+
+    @Provides
+    @Singleton
+    static Retrofit provideRetrofit(MoshiConverterFactory moshiCon) {
+        return new Retrofit.Builder()
+                .baseUrl("http://localhost/")
+                .addConverterFactory(moshiCon)
+                .build();
+    }
+
+    @Provides
+    @Singleton
+    static MoshiConverterFactory provideMoshiConverterFactory() {
+        return MoshiConverterFactory.create();
+    }
+
+    @Provides
+    @Singleton
+    static RecipeService provideRecipeService(Retrofit retrofit) {
+        return retrofit.create(RecipeService.class);
+    }
 }
