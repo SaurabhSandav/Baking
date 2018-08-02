24af3fd00e3d85ee3e57ee96176fdc0a65d51b2f
Saurabh Sandav <justmailsaurabh@gmail.com>
2018-08-02 17:05:00 +0530

    Implement DataBinding
    Signed-off-by: Saurabh Sandav <justmailsaurabh@gmail.com>

--- app/src/main/java/com/redridgeapps/baking/ui/base/BaseActivity.java
+++ app/src/main/java/com/redridgeapps/baking/ui/base/BaseActivity.java
@@ -1,14 +1,28 @@
 package com.redridgeapps.baking.ui.base;
 
+import android.databinding.DataBindingUtil;
+import android.databinding.ViewDataBinding;
 import android.os.Bundle;
+import android.support.annotation.LayoutRes;
 import android.support.annotation.Nullable;
 
 import dagger.android.support.DaggerAppCompatActivity;
 
-public abstract class BaseActivity extends DaggerAppCompatActivity {
+public abstract class BaseActivity<VDB extends ViewDataBinding> extends DaggerAppCompatActivity {
+
+    private VDB binding;
+
+    @LayoutRes
+    protected abstract int provideLayout();
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        binding = DataBindingUtil.setContentView(this, provideLayout());
+    }
+
+    protected VDB getBinding() {
+        return binding;
     }
 }
--- app/src/main/java/com/redridgeapps/baking/ui/main/MainActivity.java
+++ app/src/main/java/com/redridgeapps/baking/ui/main/MainActivity.java
@@ -3,13 +3,18 @@ package com.redridgeapps.baking.ui.main;
 import android.os.Bundle;
 
 import com.redridgeapps.baking.R;
+import com.redridgeapps.baking.databinding.ActivityMainBinding;
 import com.redridgeapps.baking.ui.base.BaseActivity;
 
-public class MainActivity extends BaseActivity {
+public class MainActivity extends BaseActivity<ActivityMainBinding> {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected int provideLayout() {
+        return R.layout.activity_main;
     }
 }
