92f902e2550b2cfd9778bdb0e5bf2aca9cfcab8a
Saurabh Sandav <justmailsaurabh@gmail.com>
2018-08-03 20:07:57 +0530

    Util: Add Event
    Signed-off-by: Saurabh Sandav <justmailsaurabh@gmail.com>

---
+++ app/src/main/java/com/redridgeapps/baking/util/Event.java
@@ -0,0 +1,20 @@
+package com.redridgeapps.baking.util;
+
+public class Event<T> {
+
+    private T payload;
+    private boolean hasBeenHandled = false;
+
+    public Event(T payload) {
+        this.payload = payload;
+    }
+
+    public boolean hasBeenHandled() {
+        return hasBeenHandled;
+    }
+
+    public T getPayload() {
+        hasBeenHandled = true;
+        return payload;
+    }
+}
