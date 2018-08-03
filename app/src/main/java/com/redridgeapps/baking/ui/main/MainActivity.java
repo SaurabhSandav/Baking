d09ea865f870cdf3bd7fea1197cdd01eae8b9667
Saurabh Sandav <justmailsaurabh@gmail.com>
2018-08-03 20:58:02 +0530

    Main: Fetch Recipes
    Signed-off-by: Saurabh Sandav <justmailsaurabh@gmail.com>

--- app/src/main/java/com/redridgeapps/baking/ui/main/MainActivity.java
+++ app/src/main/java/com/redridgeapps/baking/ui/main/MainActivity.java
@@ -1,20 +1,48 @@
 package com.redridgeapps.baking.ui.main;
 
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
 
 import com.redridgeapps.baking.R;
 import com.redridgeapps.baking.databinding.ActivityMainBinding;
+import com.redridgeapps.baking.model.Recipe;
+import com.redridgeapps.baking.repo.RecipeRepository;
 import com.redridgeapps.baking.ui.base.BaseActivity;
 
+import java.util.List;
+
+import javax.inject.Inject;
+
 public class MainActivity extends BaseActivity<ActivityMainBinding> {
+
+    @Inject
+    RecipeRepository recipeRepo;
+
+    private List<Recipe> recipeList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        observeRecipeOperations();
+        recipeRepo.fetchRecipes();
     }
 
     @Override
     protected int provideLayout() {
         return R.layout.activity_main;
+    }
+
+    private void observeRecipeOperations() {
+
+        recipeRepo.getRecipeLiveData().observe(this, newRecipeList -> recipeList = newRecipeList);
+
+        recipeRepo.getErrorLiveData().observe(this, event -> {
+            if (event == null || event.hasBeenHandled()) return;
+
+            Snackbar.make(getBinding().getRoot(), R.string.error_fetch_failed, Snackbar.LENGTH_INDEFINITE)
+                    .setAction(R.string.text_retry, view -> recipeRepo.fetchRecipes())
+                    .show();
+        });
     }
 }
