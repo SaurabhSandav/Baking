61557ba2bdb05d4aa6f7e48f04bded9ebc0c44d1
Saurabh Sandav <justmailsaurabh@gmail.com>
2018-08-06 19:37:24 +0530

    StepDetailFragment: Implement ExoPlayer
    Signed-off-by: Saurabh Sandav <justmailsaurabh@gmail.com>

--- app/src/main/java/com/redridgeapps/baking/ui/detail/step_detail/StepDetailFragment.java
+++ app/src/main/java/com/redridgeapps/baking/ui/detail/step_detail/StepDetailFragment.java
@@ -1,12 +1,22 @@
 package com.redridgeapps.baking.ui.detail.step_detail;
 
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.exoplayer2.DefaultLoadControl;
+import com.google.android.exoplayer2.DefaultRenderersFactory;
+import com.google.android.exoplayer2.ExoPlayerFactory;
+import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.source.ExtractorMediaSource;
+import com.google.android.exoplayer2.source.MediaSource;
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
+import com.google.android.exoplayer2.upstream.DefaultHttpDataSourceFactory;
+import com.google.android.exoplayer2.util.Util;
 import com.redridgeapps.baking.R;
 import com.redridgeapps.baking.databinding.FragmentStepDetailBinding;
 import com.redridgeapps.baking.model.Step;
@@ -17,6 +27,11 @@ public class StepDetailFragment extends BaseFragment<FragmentStepDetailBinding> 
     private static final String ARG_STEP = "step";
 
     private Step step;
+
+    private SimpleExoPlayer player;
+    private boolean playWhenReady;
+    private int currentWindow = 0;
+    private long playbackPosition = 0;
 
     public static StepDetailFragment newInstance(Step step) {
 
@@ -45,5 +60,63 @@ public class StepDetailFragment extends BaseFragment<FragmentStepDetailBinding> 
     @Override
     protected int provideLayout() {
         return R.layout.fragment_step_detail;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        if (Util.SDK_INT > 23) initializePlayer();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        //hideSystemUi();
+        if (Util.SDK_INT <= 23 || player == null) initializePlayer();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        if (Util.SDK_INT <= 23) releasePlayer();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (Util.SDK_INT > 23) releasePlayer();
+    }
+
+    private void initializePlayer() {
+        player = ExoPlayerFactory.newSimpleInstance(
+                new DefaultRenderersFactory(requireContext()),
+                new DefaultTrackSelector(),
+                new DefaultLoadControl()
+        );
+
+        getBinding().exoplayer.setPlayer(player);
+
+        player.setPlayWhenReady(playWhenReady);
+        player.seekTo(currentWindow, playbackPosition);
+
+        Uri uri = Uri.parse(step.getVideoURL());
+        MediaSource mediaSource = buildMediaSource(uri);
+        player.prepare(mediaSource, true, false);
+    }
+
+    private MediaSource buildMediaSource(Uri uri) {
+        return new ExtractorMediaSource
+                .Factory(new DefaultHttpDataSourceFactory(requireContext().getPackageName()))
+                .createMediaSource(uri);
+    }
+
+    private void releasePlayer() {
+        if (player != null) {
+            playbackPosition = player.getCurrentPosition();
+            currentWindow = player.getCurrentWindowIndex();
+            playWhenReady = player.getPlayWhenReady();
+            player.release();
+            player = null;
+        }
     }
 }
