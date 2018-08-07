84f423c8c28caf9ed8fa039e758d8805628ce0ab
Saurabh Sandav <justmailsaurabh@gmail.com>
2018-08-07 20:50:38 +0530

    Dagger: Setup Service Injection
    Signed-off-by: Saurabh Sandav <justmailsaurabh@gmail.com>

--- app/src/main/java/com/redridgeapps/baking/di/module/ComponentBuilder.java
+++ app/src/main/java/com/redridgeapps/baking/di/module/ComponentBuilder.java
@@ -3,10 +3,12 @@ package com.redridgeapps.baking.di.module;
 import com.redridgeapps.baking.di.scope.PerActivity;
 import com.redridgeapps.baking.di.scope.PerBroadcastReceiver;
 import com.redridgeapps.baking.di.scope.PerFragment;
+import com.redridgeapps.baking.di.scope.PerService;
 import com.redridgeapps.baking.ui.detail.DetailActivity;
 import com.redridgeapps.baking.ui.detail.recipe_step.RecipeStepFragment;
 import com.redridgeapps.baking.ui.detail.step_detail.StepDetailFragment;
 import com.redridgeapps.baking.ui.main.MainActivity;
+import com.redridgeapps.baking.ui.widgets.ingredient.IngredientsListWidgetService;
 import com.redridgeapps.baking.ui.widgets.ingredient.RecipeIngredientsWidget;
 
 import dagger.Module;
@@ -40,4 +42,10 @@ public abstract class ComponentBuilder {
     @PerBroadcastReceiver
     @ContributesAndroidInjector()
     public abstract RecipeIngredientsWidget bindRecipeIngredientsWidget();
+
+    // Services
+
+    @PerService
+    @ContributesAndroidInjector()
+    public abstract IngredientsListWidgetService bindIngredientsListWidgetService();
 }
